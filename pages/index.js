dc32268a3f6195c508a0bb970c4430e239a9c49a
Brandon LeBoeuf <brandon.leboeuf@gmail.com>
2020-12-06 12:15:17 -0800

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
+import styles from "../styles/Home.module.css";
 
-export default function Home() {
+export default function Home({ launches }) {
+  console.log("launchs", launches);
   return (
     <div className={styles.container}>
       <Head>
@@ -10,56 +12,102 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <h1 className={styles.title}>SpaceX Launches</h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+        <p className={styles.description}>Latest launches from SpaceX</p>
+        <p>GraphQL will be used to get data from:</p>
+        <code className={styles.code}>https://spacex.land/</code>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          {!launches && "Loading..."}
+          {launches.map((launch) => {
+            return (
+              <a
+                key={launch.id}
+                href={launch.links.video_link}
+                href={launch.links.article_link}
+                className={styles.card}
+              >
+                <div>
+                  <h3>Mission Name: {launch.mission_name}</h3>
+                  <p>
+                    <strong>Rocket Type:</strong> {launch.rocket.rocket_name}
+                  </p>
+                  <img
+                    src={launch.links.mission_patch}
+                    style={{
+                      maxWidth: "200px",
+                      maxHeight: "200px",
+                      margin: "15px",
+                    }}
+                  />
+                  <p>
+                    <strong>Launch Time: </strong>
+                    {new Date(launch.launch_date_local).toLocaleDateString(
+                      "en-US"
+                    )}
+                  </p>
+                  <div>
+                    <p>
+                      <strong>Details: </strong>
+                      {launch.details ? launch.details : "N/A"}
+                    </p>
+                  </div>
+                </div>
+              </a>
+            );
+          })}
         </div>
       </main>
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://brandon-leboeuf.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          built by Ⓑrandon
         </a>
       </footer>
     </div>
-  )
+  );
+}
+
+export async function getStaticProps() {
+  const client = new ApolloClient({
+    uri: "https://api.spacex.land/graphql",
+    cache: new InMemoryCache(),
+  });
+
+  const { data } = await client.query({
+    query: gql`
+      {
+        launchesPast(limit: 10) {
+          id
+          mission_name
+          launch_date_local
+          launch_site {
+            site_name_long
+          }
+          links {
+            article_link
+            video_link
+            mission_patch
+          }
+          rocket {
+            rocket_name
+          }
+          details
+        }
+      }
+    `,
+  });
+
+  // console.log("data", data);
+
+  return {
+    props: {
+      launches: data.launchesPast,
+    },
+  };
 }
